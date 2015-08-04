1bf9194979d04d1a9131b16406ef8493b2dd1d2b
GameCharmer <admin@gamecharmer.com>
2015-08-04 10:04:25 -0400

    Add comment handling and get working database name function.  Ran code cleaner

--- src/Database.php
+++ src/Database.php
@@ -10,7 +10,8 @@ class Database extends PDO
     /**
      * @return bool
      */
-    public function ping() {
+    public function ping()
+    {
         try {
             $this->query('SELECT 1');
         } catch (PDOException $e) {
@@ -20,10 +21,12 @@ class Database extends PDO
     }
 
     /**
+     * Note: This is injectible!  Use with caution!
      * @param string $table
      * @return bool
      */
-    public function tableExists($table) {
+    public function tableExists($table)
+    {
         try {
             $result = $this->query('SELECT 1 FROM '.$table.' LIMIT 1');
         } catch (PDOException $e) {
@@ -32,5 +35,50 @@ class Database extends PDO
         $good = ($result !== false);
         $result->closeCursor();
         return $good;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWorkingDatabaseName()
+    {
+        $stmt = $this->query('select database()');
+        $name = $stmt->fetchColumn();
+        $stmt->closeCursor();
+        return $name;
+    }
+
+    /**
+     * @param string $table
+     * @return bool|string
+     */
+    public function getTableComment($table)
+    {
+        $schema = $this->getWorkingDatabaseName();
+
+        $sql = 'SELECT TABLE_COMMENT AS comment
+                FROM information_schema.TABLES
+                WHERE TABLE_NAME = :table
+                AND TABLE_SCHEMA = :schema';
+        $stmt = $this->query($sql);
+        $stmt->execute(array(':table'=>$table, ':schema'=>$schema));
+        $row = $stmt->fetch();
+        $stmt->closeCursor();
+        if (is_array($row)) {
+            return $row['comment'];
+        }
+        return false;
+    }
+
+    /**
+     * @param string $table
+     * @param string $comment
+     */
+    public function setTableComment($table, $comment)
+    {
+        $sql = 'ALTER TABLE '.$table.' COMMENT = :comment';
+        $stmt = $this->query($sql);
+        $stmt->execute(array(':table'=>$table, ':comment'=>$comment));
+        $stmt->closeCursor();
     }
 }
