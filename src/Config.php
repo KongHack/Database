8b19caa9b106979e4559970693e5ef0ec813f001
GameCharmer <admin@gamecharmer.com>
2016-05-11 10:25:09 -0400

    Config adjustment to not blow away settings!  The write_ini thing only works for flat files, not ones with structure

--- src/Config.php
+++ src/Config.php
@@ -26,23 +26,6 @@ class Config
             throw new Exception('Config does not contain "common" value!');
         }
 
-        // Get the example config, make sure we have all variables.
-        $example = rtrim(dirname(__FILE__), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR;
-        $example .= 'config/config.example.ini';
-        $exConfig = parse_ini_file($example);
-
-        $reSave = false;
-        foreach ($exConfig as $k => $v) {
-            if (!isset($config[$k])) {
-                $config[$k] = $v;
-                $reSave     = true;
-            }
-        }
-
-        if ($reSave) {
-            $this->writeIniFile($config, $file);
-        }
-
         $this->config = $config;
     }
 
@@ -52,54 +35,5 @@ class Config
     public function getConfig()
     {
         return $this->config;
-    }
-
-    /**
-     * @url          http://stackoverflow.com/questions/1268378/create-ini-file-write-values-in-php
-     * @param array  $assoc_arr
-     * @param string $path
-     * @param bool   $has_sections
-     * @return bool|int
-     */
-    private function writeIniFile($assoc_arr, $path, $has_sections = false)
-    {
-        $content = "";
-        if ($has_sections) {
-            foreach ($assoc_arr as $key => $elem) {
-                $content .= "[".$key."]\n";
-                foreach ($elem as $key2 => $elem2) {
-                    if (is_array($elem2)) {
-                        for ($i = 0; $i < count($elem2); $i++) {
-                            $content .= $key2."[] = \"".$elem2[$i]."\"\n";
-                        }
-                    } elseif ($elem2 == "") {
-                        $content .= $key2." = \n";
-                    } else {
-                        $content .= $key2." = \"".$elem2."\"\n";
-                    }
-                }
-            }
-        } else {
-            foreach ($assoc_arr as $key => $elem) {
-                if (is_array($elem)) {
-                    for ($i = 0; $i < count($elem); $i++) {
-                        $content .= $key."[] = \"".$elem[$i]."\"\n";
-                    }
-                } elseif ($elem == "") {
-                    $content .= $key." = \n";
-                } else {
-                    $content .= $key." = \"".$elem."\"\n";
-                }
-            }
-        }
-
-        if (!$handle = fopen($path, 'w')) {
-            return false;
-        }
-
-        $success = fwrite($handle, $content);
-        fclose($handle);
-
-        return $success;
     }
 }
